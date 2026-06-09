4497dd45ca61bb23dd338777d0a8ad12ab7612e4
Davion-cyber <kelvinquartey3@gmail.com>
2026-06-09 18:06:37 +0000

    Change redirects to HTML responses in spotify-callback

--- api/spotify-callback.js
+++ api/spotify-callback.js
@@ -1,19 +1,14 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
   const { code, error } = req.query;
-
   if (error || !code) {
-    return res.redirect('moodlift://spotify-auth?error=access_denied');
+    return res.send(`<html><body><script>window.location.href = 'moodlift://spotify-auth?error=access_denied';</script></body></html>`);
   }
-
   try {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
     const redirectUri = 'https://moodlift-backend.vercel.app/api/spotify-callback';
-
     const credentials = Buffer.from(clientId + ':' + clientSecret).toString('base64');
-
     const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
       headers: {
@@ -22,20 +17,25 @@ export default async function handler(req, res) {
       },
       body: 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + encodeURIComponent(redirectUri),
     });
-
     const tokenData = await tokenRes.json();
-
     if (!tokenData.access_token) {
-      return res.redirect('moodlift://spotify-auth?error=token_failed');
+      return res.send(`<html><body><script>window.location.href = 'moodlift://spotify-auth?error=token_failed';</script></body></html>`);
     }
-
-    return res.redirect(
-      'moodlift://spotify-auth?access_token=' + tokenData.access_token +
-      '&refresh_token=' + tokenData.refresh_token +
-      '&expires_in=' + tokenData.expires_in
-    );
-
+    const deepLink = 'moodlift://spotify-auth?access_token=' + tokenData.access_token + '&refresh_token=' + tokenData.refresh_token + '&expires_in=' + tokenData.expires_in;
+    return res.send(`
+      <html>
+        <body>
+          <script>
+            window.location.href = '${deepLink}';
+            setTimeout(() => {
+              document.body.innerHTML = '<p>Redirecting back to MoodLift...</p>';
+            }, 500);
+          </script>
+          <p>Connecting to MoodLift...</p>
+        </body>
+      </html>
+    `);
   } catch (err) {
-    return res.redirect('moodlift://spotify-auth?error=server_error');
+    return res.send(`<html><body><script>window.location.href = 'moodlift://spotify-auth?error=server_error';</script></body></html>`);
   }
 }
